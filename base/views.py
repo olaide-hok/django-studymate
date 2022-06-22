63fcae771f69e392758365841c969c71ac3606df
olaide-hok <olaidekareem43@yahoo.com>
2022-06-22 18:56:57 +0100

    Setup custom User
    Define a class based user which inherits from django AbstractUser class.

--- base/views.py
+++ base/views.py
@@ -3,21 +3,13 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
 
 # Create your views here.
-
-# rooms = [
-#     {'id': 1, 'name': 'Lets learn python!'},
-#     {'id': 2, 'name': 'Design with me'},
-#     {'id': 3, 'name': 'Frontend developers'},
-# ]
-
 
 def loginPage(request):
     page = 'login'
