f3abc7f63bbead2d8f43b9bd9af19c7377ea178a
olaide-hok <olaidekareem43@yahoo.com>
2022-06-23 08:56:48 +0100

    Create MyUserCreationForm
    MyUserCreation use django userCreationForm.
    
    Add name, avatar and bio to UserForm class fields.
    
    Change UserCreationForm to Custom MyUserCreationForm.
    
    Change username to email to login page post request.
    
    Change registerPage form to MyUserCreationForm class.
    
    Change  activity component, img avatar to user provided avatar.
    
    Change feed component, img avatar to user provided avatar.
    
    Change login register from username field to email field.
    
    Change navbar img avatar to user provided avatar.

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,11 @@
-from dataclasses import field
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from .models import Room, User
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
 
 class RoomForm(ModelForm):
     class Meta:
@@ -11,4 +16,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['name', 'avatar', 'bio', 'username', 'email']
